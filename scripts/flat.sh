bea29b2c7ce8b6072eb9214c6fb0cbcde44bef7d
reed-hong <fishwarter@163.com>
2020-08-20 14:56:45 +0800

    用一套策略代码实现dforce多个稳定币的挖矿

--- scripts/flat.sh
+++ scripts/flat.sh
@@ -2,17 +2,12 @@ echo "deploy begin....."
 
 TF_CMD=node_modules/.bin/truffle-flattener
 
-TOKEN_LIST=(yVaultUSDC StrategyDForceUSDC)
+TOKEN_LIST=( StrategyDForce)
 for contract in ${TOKEN_LIST[@]};
 do
     echo $contract
     $TF_CMD ./contracts/dforce/$contract.sol > $contract.full.sol
-    solcjs --bin --abi --optimize $contract.full.sol
     mv $contract.full.sol ./deployments/$contract.full.sol 
-    mv $contract_full_sol_$contract.bin ./deployments/$contract.full.bin
-    mv $contract_full_sol_$contract.abi ./deployments/$contract.full.abi
 done
-
-rm *_sol_*
 
 echo "deploy  end....."
